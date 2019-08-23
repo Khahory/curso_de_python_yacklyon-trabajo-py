b093ff24fcbcfc857f34120ee1ca942b3afd0e94
angelmaria <angeld05@live.com>
2019-08-23 10:31:32 -0400

    CURSO DE PYTHON 2019 | #6 |🐍 TIPOS DE DATOS

--- ClaseUno.py
+++ ClaseUno.py
@@ -1,1 +1,17 @@
-print (3)
+#convertir a String
+print (str('hola'))
+print (str(120))
+
+# Convertir a int
+print (int(10))
+print (int("21"))
+
+# Convertir a float
+print (float(30))
+print (float(22.22))
+
+
+
+
+
+
