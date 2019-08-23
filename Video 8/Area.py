3a4efc05019f407228418fa0627b812e5a036b23
angelmaria <angeld05@live.com>
2019-08-23 11:05:32 -0400

    #9 🐍 OPERADORES RELACIONALES

---
+++ Video 8/Area.py
@@ -0,0 +1,6 @@
+#Buscar el area de un triangulo
+
+# Datos
+B = int(input("Digita la base \n"))
+H = int(input("Digita la altura \n"))
+print ((B*H)/2)
---
+++ Video 8/Promedio.py
@@ -0,0 +1,8 @@
+#Encontrar el promedio
+
+nota1 = int(input("Nota 1 \n"))
+nota2 = int(input("Nota 2 \n"))
+nota3 = int(input("Nota 3 \n"))
+res = (nota1+nota2+nota3)/3
+
+print ("El promedio es: " +str(res))
