42745b8156af45e12c1985fc1f4a0884dac2fed8
angelmaria <angeld05@live.com>
2019-08-23 12:12:04 -0400

    #11 |🐍 CONDICIONALES I (if - else)

---
+++ Video 11/Condicionales.py
@@ -0,0 +1,8 @@
+#Aplicando if else
+a = int(input("Digita el valor a \n"))
+b = int(input("Digita el valor b \n"))
+
+if a > b:
+    print ("El valor de: " +str(a) +" es mayor que: " +str(b))
+else:
+    print ("El valor de: " + str(b) + " es mayor que: " + str(a))
