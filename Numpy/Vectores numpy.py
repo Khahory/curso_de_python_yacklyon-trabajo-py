c497179282d4af38fab042c1a39555829c68ceec
angelmaria <angeld05@live.com>
2019-08-26 16:30:46 -0400

    #36 🐍 VECTORES Y MATRICES v2

--- Numpy/Vectores numpy.py
+++ Numpy/Vectores numpy.py
@@ -16,3 +16,14 @@ a = np.array([3,1,5,6,21])
 print(a.argmax()) # Obtiene la posicion del numero mas alto
 
 print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
+
+a = np.array([3,1,5,6,21])
+print(a.prod()) # Multiplica todos los numeros dentro del vector
+
+print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
+
+matrizA = np.array([[3,1,5,6,21],[3,1,5,6,21],[3,1,5,6,21]])
+matrizB = np.array([[3,1,5,6,21],[3,1,5,6,21],[3,1,5,6,21]])
+print(matrizA + matrizB)
+
+print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
