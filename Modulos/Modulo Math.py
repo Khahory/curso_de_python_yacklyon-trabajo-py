a6c3e2e573e0b71fc70fab48647325315547c253
angelmaria <angeld05@live.com>
2019-08-26 16:49:56 -0400

    #37 🐍 MODULOS v3

--- Modulos/Modulo Math.py
+++ Modulos/Modulo Math.py
@@ -2,8 +2,19 @@
 
 from math import log
 from random import choice
+from datetime import date
+from fractions import Fraction
+
 print(log(10000, 10))
 
 print(choice(['Cara', "Cruz"]))
+
+dia = date(2019,2,22)
+print(dia)
+
+a = Fraction(2,4)
+b = Fraction(4,8)
+
+print(Fraction(a+b))
 
 
