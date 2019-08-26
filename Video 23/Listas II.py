d07a0cb9991c128c92783e2e93d5af95708f96d7
angelmaria <angeld05@live.com>
2019-08-26 10:51:41 -0400

    #23 |🐍 LISTAS v4

--- Video 23/Listas II.py
+++ Video 23/Listas II.py
@@ -1,9 +1,13 @@
 a = [1,2,3]
 b = [4,3,2]
+c = [10,20,30]
 
-print(a+b)
+print(a+b+c)
 
 #lista dentro de una lista
 
-listaSuper = [a,b]
+listaSuper = [a,b,c]
 print(listaSuper)
+
+#buscar filas columnas
+print(listaSuper[2][0]) #[fila][columna] de la lista
