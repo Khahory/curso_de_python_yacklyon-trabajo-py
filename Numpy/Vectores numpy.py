d765752ba4523debd5146ad153a0ec721ef332c1
angelmaria <angeld05@live.com>
2019-08-26 16:18:46 -0400

    #36 🐍 VECTORES Y MATRICES v1

--- Numpy/Vectores numpy.py
+++ Numpy/Vectores numpy.py
@@ -11,3 +11,8 @@ b = np.array([3,1,5,6,21])
 print(a>b) # Verifica con sus respectivos indices
 
 print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
+
+a = np.array([3,1,5,6,21])
+print(a.argmax()) # Obtiene la posicion del numero mas alto
+
+print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
