d11bf623037fd6fd15186e4931c703e84f4bb2ff
angelmaria <angeld05@live.com>
2019-08-26 14:23:21 -0400

    #32 🐍 DICCIONARIOS v2

--- Diccionarios/Diccionarios II.py
+++ Diccionarios/Diccionarios II.py
@@ -25,3 +25,15 @@ edades = {"Angel":20, "Jason":18, "Aura":12, "Maria":40}
 print(edades.items()) #Agrupados en pares
 
 print("\033[;36m"+"-------------------------------------"+'\033[0;m')
+
+edades = {"Angel":20, "Jason":18, "Aura":12, "Maria":40}
+print(edades.keys()) #Muestra las claves
+
+print("\033[;36m"+"-------------------------------------"+'\033[0;m')
+
+notas1 = {"a":1, "b":2, "c":343}
+notas2 = {"c":10, "d":5, "e":6}
+notas1.update(notas2)
+print(notas1)
+
+print("\033[;36m"+"-------------------------------------"+'\033[0;m')
