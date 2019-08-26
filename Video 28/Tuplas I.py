e30e5889894e6eb8cd6082364025ae897b3d2b40
angelmaria <angeld05@live.com>
2019-08-26 12:43:03 -0400

    #28 🐍 TUPLAS

---
+++ Video 28/Tuplas I.py
@@ -0,0 +1,8 @@
+#Tuplas
+a = 10
+b = 5
+print(a)
+
+(a,b) = (b,a)
+
+print(a) #Cambio los valores con las tuplas
---
+++ Video 28/Tuplas II.py
@@ -0,0 +1,4 @@
+#Tuplas
+
+ejemplo = (10, "Angel", 4)
+print(ejemplo[1]) #Busca el de la posicion 1
