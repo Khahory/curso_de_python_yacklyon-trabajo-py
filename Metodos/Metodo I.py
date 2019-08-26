25d13aec11c99acf946bef16defd2076d9ffb355
angelmaria <angeld05@live.com>
2019-08-26 11:54:58 -0400

    #26 |🐍 METODOS II v2

--- Metodos/Metodo I.py
+++ Metodos/Metodo I.py
@@ -1,4 +1,12 @@
 lista = [2,7,8,14,8]
-print(max(lista)) #Busca el mayor elemento
-print(min(lista)) #Busca el menor elemento
-print(len(lista)) #Cuenta los elementos
+# print(max(lista)) #Busca el mayor elemento
+# print(min(lista)) #Busca el menor elemento
+# print(len(lista)) #Cuenta los elementos
+
+lista.sort() #Ordena la lista
+lista.reverse() #invierte la lista
+lista.count(2) # Cuenta un elemento dentro de la lista
+lista.append(100) #Agrega un elemento a la lista
+
+print(lista.count(100))
+print(lista)
