392f651cd1faba61f46be3e5cec518b7f8339f30
angelmaria <angeld05@live.com>
2019-08-26 16:12:48 -0400

    #36 🐍 VECTORES Y MATRICES v1

---
+++ Numpy/Vectores numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([3,6,2,6,2])
+b = np.array([3,1,5,6,21])
+print(a+b) # se suman con sus respectivos indices
+
+print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
+
+a = np.array([3,6,2,6,2])
+b = np.array([3,1,5,6,21])
+print(a>b) # Verifica con sus respectivos indices
+
+print("\033[1;33m"+"----------------------------------------"+'\033[0;m')
