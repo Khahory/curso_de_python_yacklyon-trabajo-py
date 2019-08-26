2731e0f8fd554241866047e7ab6d0765e3663d79
angelmaria <angeld05@live.com>
2019-08-26 13:15:02 -0400

    #30 🐍 SET (conjuntos) v2

--- Set conjuntos/Conjuntos.py
+++ Set conjuntos/Conjuntos.py
@@ -3,3 +3,13 @@
 a = {3,3,1,5,2,5, "b", "g", "z", "a"} #No siempre estan ordenados
 # Los valores iguales no se repiten
 print(a)
+
+#--------------------------------------------------------
+
+alumno = set()
+alumno = {"Angel", "Jason", "Aura"}
+
+if "Angel" in alumno:
+    print(True)
+
+print("Jasonnnnnnnn" in alumno)
